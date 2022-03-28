850797db1a1178f2948f6839ea85a408f25cc8ff
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-28 09:58:45 +0200

    the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './components/template-driven/template-driven.component';
+import { ReactiveComponent } from './components/reactive/reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenComponent
+    TemplateDrivenComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'driven', component: TemplateDrivenComponent },
+      { path: 'reactive', component: ReactiveComponent }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
